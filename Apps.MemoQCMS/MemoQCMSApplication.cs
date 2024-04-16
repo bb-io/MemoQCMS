8dd168225a5cc2c663d702e52f133b1b41cbfc94
bZverok <137277669+bZverok@users.noreply.github.com>
2024-04-16 18:51:04 +0300

    Categories (#5)
    * CI: update workflow to invoke DEV deployment
    
    * CI: update pipeline to include dispatch event
    
    * CI: invoke Dev workflow once manually triggered
    
    * added categories
    
    * Update Apps.MemoQCMS.csproj
    
    ---------
    
    Co-authored-by: Ihor Kriatov <ihor.kriatov@blackbird.io>

--- Apps.MemoQCMS/MemoQCMSApplication.cs
+++ Apps.MemoQCMS/MemoQCMSApplication.cs
@@ -1,9 +1,16 @@
 ﻿using Blackbird.Applications.Sdk.Common;
+using Blackbird.Applications.Sdk.Common.Metadata;
 
 namespace Apps.MemoQCMS;
 
-public class MemoQCMSApplication : IApplication
+public class MemoQCMSApplication : IApplication, ICategoryProvider
 {
+    public IEnumerable<ApplicationCategory> Categories
+    {
+        get => [ApplicationCategory.CatAndTms];
+        set { }
+    }
+    
     public string Name
     {
         get => "memoQ CMS";
