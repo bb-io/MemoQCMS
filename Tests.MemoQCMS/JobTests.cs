d2d07efaf5f863c93570a2ac911faf6a8a9e3168
Mihaly Hobor <mihaly.hobor@blackbird.io>
2025-01-30 14:28:00 +0100

    Extend and fix JobTests

--- Tests.MemoQCMS/JobTests.cs
+++ Tests.MemoQCMS/JobTests.cs
@@ -26,16 +26,30 @@ namespace Tests.MemoQCMS
         }
 
         [TestMethod]
+        public async Task CreateJob_TargetLanguageNotValid_MisconfigurationException()
+        {
+            var actions = new JobActions(InvocationContext, FileManager);
+
+            var orderidentifier = new OrderIdentifier() { OrderId = "241204-FVY" };
+
+            var input = new CreateJobRequest() { Name = "testname",SourceLanguage = "eng", TargetLanguage = "agsd" };
+            var file = new Apps.MemoQCMS.Models.FileWrapper() { File = new Blackbird.Applications.Sdk.Common.Files.FileReference() { Name = "testfile.txt", ContentType = "text/plain" } };
+
+            await Throws.MisconfigurationException(() => actions.CreateJob(orderidentifier,input, file));
+
+        }
+
+        [TestMethod]
         public async Task CreateJob_TargetLanguageNull_MisconfigurationException()
         {
             var actions = new JobActions(InvocationContext, FileManager);
 
             var orderidentifier = new OrderIdentifier() { OrderId = "241204-FVY" };
 
-            var input = new CreateJobRequest() { Name = "testname",SourceLanguage = "eng", TargetLanguage = null };
+            var input = new CreateJobRequest() { Name = "testname", SourceLanguage = "eng", TargetLanguage = null };
             var file = new Apps.MemoQCMS.Models.FileWrapper() { File = new Blackbird.Applications.Sdk.Common.Files.FileReference() { Name = "testfile.txt", ContentType = "text/plain" } };
 
-            await Throws.MisconfigurationException(() => actions.CreateJob(orderidentifier,input, file));
+            await Throws.MisconfigurationException(() => actions.CreateJob(orderidentifier, input, file));
 
         }
     }
